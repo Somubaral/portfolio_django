35d8760effd3eeba5fb779cbdeedda1c0b51b0df
saumyakant baral <saumyakantbaral2001@gmail.com>
2026-02-16 04:08:52 +0530

    error in deployment and req.txt file

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'portfolio_django.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    
 }
 
 # Password validation
