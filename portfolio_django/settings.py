e00876876d32487e4f1cd9ee1fced912bdd14cb3
saumyakant baral <saumyakantbaral2001@gmail.com>
2026-02-16 03:34:17 +0530

    change in settings.py file - add and connect PostgreSQL to the django

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -79,13 +79,18 @@ WSGI_APPLICATION = 'portfolio_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
